9cb3ade2b12aa8b37c389bdde946b50d94f912f6
fangliang <f304646673@gmail.com>
2024-09-27 15:43:26 +0800

    Refactor solution to track the number of people instead of using a stack

--- 3168-Minimum-Number-of-Chairs-in-a-Waiting-Room/cplusplus/src/solution.hpp
+++ 3168-Minimum-Number-of-Chairs-in-a-Waiting-Room/cplusplus/src/solution.hpp
@@ -1,18 +1,17 @@
 #include <string>
-#include <stack>
 using namespace std;
 
 class Solution {
 public:
     int minimumChairs(string s) {
         int maxChairs = 0;
-        stack<int> chairs;
+        int person = 0;
         for (char c : s) {
             if (c == 'L') {
-                chairs.pop();
+                person--;
             } else {
-                chairs.push(1);
-                maxChairs = max(maxChairs, (int)chairs.size());
+                person++;
+                maxChairs = max(maxChairs, person);
             }
         }
         return maxChairs;

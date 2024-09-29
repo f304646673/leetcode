cac1b50e9005ecc1e2cdb09494ccbfeb3e4a633f
fangliang <f304646673@gmail.com>
2024-09-29 15:50:05 +0800

    Refactor test cases to check if grid satisfies conditions

--- 3142-Check-if-Grid-Satisfies-Conditions/cplusplus/test/test.cpp
+++ 3142-Check-if-Grid-Satisfies-Conditions/cplusplus/test/test.cpp
@@ -3,5 +3,24 @@
 
 TEST(Solution, Test1) {
     Solution solution;
-    vector<int> v1 = {1,0,2};
+    vector<vector<int>> grid = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+    bool expected = true;
+    bool result = solution.satisfiesConditions(grid);
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Solution, Test2) {
+    Solution solution;
+    vector<vector<int>> grid = {{1, 2, 3}, {1, 2, 3}, {1, 2, 2}};
+    bool expected = false;
+    bool result = solution.satisfiesConditions(grid);
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Solution, Test3) {
+    Solution solution;
+    vector<vector<int>> grid = {{1, 2, 3}, {1, 2, 3}, {1, 2, 4}};
+    bool expected = false;
+    bool result = solution.satisfiesConditions(grid);
+    ASSERT_EQ(expected, result);
 }

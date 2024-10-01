c964e1cf207c237afd85ec0f31ee8893df3ebf34
fangliang <f304646673@gmail.com>
2024-10-01 12:11:17 +0800

    Refactor Solution class to find the difference between the minimum elements of two vectors

--- 3131-Find-the-Integer-Added-to-Array-I/cplusplus/src/solution.hpp
+++ 3131-Find-the-Integer-Added-to-Array-I/cplusplus/src/solution.hpp
@@ -1,10 +1,14 @@
 #include <vector>
-#include <numeric>
+#include <numeric>  // for std::accumulate
+#include <algorithm> // for std::min_element
 using namespace std;
 
 class Solution {
 public:
+    // int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+    //     return (accumulate(nums2.begin(), nums2.end(), 0) - accumulate(nums1.begin(), nums1.end(), 0)) / static_cast<long>(nums1.size());
+    // }
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
-        return (accumulate(nums2.begin(), nums2.end(), 0) - accumulate(nums1.begin(), nums1.end(), 0)) / static_cast<long>(nums1.size());
+        return *min_element(nums2.begin(), nums2.end()) - *min_element(nums1.begin(), nums1.end());
     }
 };
